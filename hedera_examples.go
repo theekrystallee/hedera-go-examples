df3ff8a5fa5a167878b6051c4c234dfae39e3c80
theekrystallee <krystal@swirldslabs.com>
2024-05-21 17:05:10 -0700

    initial commit

---
+++ hedera_examples.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hashgraph/hedera-sdk-go/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	//Loads the .env file and throws an error if it cannot load the variables from that file correctly
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(fmt.Errorf("Unable to load environment variables from .env file. Error:\n%v\n", err))
+	}
+
+	//Grab your testnet account ID and private key from the .env file
+	myAccountId, err := hedera.AccountIDFromString(os.Getenv("MY_ACCOUNT_ID"))
+	if err != nil {
+		panic(err)
+	}
+
+	myPrivateKey, err := hedera.PrivateKeyFromString(os.Getenv("MY_PRIVATE_KEY"))
+	if err != nil {
+		panic(err)
+	}
+
+	//Print your testnet account ID and private key to the console to make sure there was no error
+	fmt.Printf("\nThe account ID is = %v\n", myAccountId)
+	fmt.Printf("\nThe private key is = %v\n", myPrivateKey)
+	fmt.Printf("\n")
+}
