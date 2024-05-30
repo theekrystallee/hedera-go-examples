c00c358881e2d46e97168ba3e91f55e9eff28ba1
theekrystallee <krystal@swirldslabs.com>
2024-05-29 17:08:35 -0700

    initial commit

---
+++ create_an_account.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hashgraph/hedera-sdk-go/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	//Loads the .env file and throws an error if it cannot load the variables from that file co
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(fmt.Errorf("Unable to load environment variables from .env file. Error:\n%v\n", e
+	}
+
+	//Grab your testnet account ID and private key from the .env file
+	myAccountId, err := hedera.AccountIDFromString(os.Getenv("MY_ACCOUNT_ID"))
+	if err != nil {
+		panic(err)
+	}
+
+	myPrivateKey, err := hedera.PrivateKeyFromString(os.Getenv("MY_PRIVATE_KEY"))
+	if err != nil {
+		panic(err)
+	}
+
+	//Create your testnet client
+	client := hedera.ClientForTestnet()
+	client.SetOperator(myAccountId, myPrivateKey)
+
+	// Set default max transaction fee & max query payment
+	client.SetDefaultMaxTransactionFee(hedera.HbarFrom(100, hedera.HbarUnits.Hbar))
+	client.SetDefaultMaxQueryPayment(hedera.HbarFrom(50, hedera.HbarUnits.Hbar))
+
+	//Generate new keys for the account you will create
+	newAccountPrivateKey, err := hedera.PrivateKeyGenerateEd25519()
+	if err != nil {
+		panic(err)
+	}
+
+	newAccountPublicKey := newAccountPrivateKey.PublicKey()
+
+	//Create new account and assign the public key
+	newAccount, err := hedera.NewAccountCreateTransaction().
+		SetKey(newAccountPublicKey).
+		SetInitialBalance(hedera.HbarFrom(1000, hedera.HbarUnits.Tinybar)).
+		Execute(client)
+
+	//Request the receipt of the transaction
+	receipt, err := newAccount.GetReceipt(client)
+	if err != nil {
+		panic(err)
+	}
+
+	//Get the new account ID from the receipt
+	newAccountId := *receipt.AccountID
+
+	//Print the new account ID to the console
+	fmt.Println("\n")
+	fmt.Printf("New account ID: %v\n", newAccountId)
+
+	//Create the account balance query
+	query := hedera.NewAccountBalanceQuery().
+		SetAccountID(newAccountId)
+
+	//Sign with client operator private key and submit the query to a Hedera network
+	accountBalance, err := query.Execute(client)
+	if err != nil {
+		panic(err)
+	}
+
+	//Print the balance of tinybars
+	fmt.Println("New account balance for the new account is", accountBalance.Hbars.AsTinybar())
+}
